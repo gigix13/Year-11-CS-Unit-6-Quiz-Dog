6eddac8b66f2623a7703aa3a69086847e09b8807
gigixiao <gigix2025@student.cis.edu.hk>
2023-02-14 14:44:14 +0800

    added dog

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,47 @@
 public class Dog {
+    private int age;
+    private String name;
+    private String color;
+
+    public void dog(){
+        age = 0;
+        color = "";
+    }
+
+    public Dog(int age, String name, String color){
+        this.age = age;
+        this.name = name;
+        this.color = color;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getColor(){
+        return color;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    private void addOneAge(){
+        age = age + 1;
+    }
+    public int birthday(){
+        addOneAge();
+        return age;
+    }
+    public String toString(){
+        return "The dog is named " + name + " and is " + age + " years old. This dog is " + color + ".";
+    }
+
+
 }
